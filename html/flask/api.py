8046912ea34acb51662e0d1be42c471da58425ae
jamesgawley <James@Jamess-Mac-mini-3.local>
2018-08-31 06:24:48 -0400

    Moved flask app

---
+++ html/flask/api.py
@@ -0,0 +1,35 @@
+import subprocess
+import csv
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+with open('data/common/cts_list.py','r') as inf:
+    cts_list = eval(inf.read())
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/oldsearch/')
+def old_read_bin():
+    result = subprocess.run(["perl", "/Users/James/tesserae/cgi-bin/read_bin_tmv.pl", "--path", "tesresults", "--export", "html"], stdout=subprocess.PIPE)
+    return result.stdout
+
+@app.route('/search/<target>/<source>/')
+def read_bin(target, source):
+    path = os.path.join('search', target, source)
+    target_name = cts_list[target]
+    source_name = cts_list[source]
+    if not os.path.exists(path):
+        subprocess.run(["perl", "cgi-bin/read_table.pl", "--target", target_name, "--source", source_name, "--binary", path])
+    result = subprocess.run(["perl", "cgi-bin/read_bin_tmv.pl", "--path", path, "--export", "html", "--window", "5"], stdout=subprocess.PIPE)
+    return result.stdout
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
