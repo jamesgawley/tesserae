6067ba9cc4fb51e9df38770da4d3c9ce08165184
jamesgawley <james.gawley@gmail.com>
2018-09-04 23:55:31 +0000

    Prep api for presentation

--- flask/api.py
+++ flask/api.py
@@ -19,14 +19,14 @@ def old_read_bin():
     result = subprocess.run(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", "tesresults", "--export", "html"], stdout=subprocess.PIPE)
     return result.stdout
 
-@app.route('/search/<target>/<source>/')
-def read_bin(target, source):
+@app.route('/search/<target>/<source>/<format>')
+def read_bin(target, source, format):
     path = os.path.join('search', target, source)
     target_name = cts_list[target]
     source_name = cts_list[source]
     if not os.path.exists(path):
         subprocess.run(["perl", "/var/www/tesserae/cgi-bin/read_table.pl", "--target", target_name, "--source", source_name, "--binary", path])
-    result = subprocess.run(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", path, "--export", "html", "--window", "5"], stdout=subprocess.PIPE)
+    result = subprocess.run(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", path, "--export", format, "--window", "5"], stdout=subprocess.PIPE)
     return result.stdout
 
 @app.route('/hello/')
