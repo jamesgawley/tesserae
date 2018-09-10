1a9ce9c0b566b023a6b616f80913cc0708e64461
jamesgawley <James@Jamess-Mac-mini-3.local>
2018-09-10 09:25:03 -0400

    Fix last.php typo

--- html/last.php
+++ html/last.php
@@ -4,7 +4,7 @@
                      <a href="http://buffalo.edu/"><img src="/images/ublogo.png" class="logo_wide" alt="University at Buffalo"></a>
                  </li>
                  <li>
-                     <a href="http://www.unive.it/pag/13526/"><img src="/images/fosc.png" class="logo_wide" alt="Ca' Foscari University of Venice"></a>
+                     <a href="http://www.unive.it/pag/13526/"><img src="/images/fosc.jpg" class="logo_wide" alt="Ca' Foscari University of Venice"></a>
                  </li>
              </ul>
 			</div>
