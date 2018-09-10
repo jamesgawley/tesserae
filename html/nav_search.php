7089c7b11b8376103972e6b237bcae8ff0bf5fd6
jamesgawley <James@Jamess-Mac-mini-3.local>
2018-09-10 09:14:27 -0400

    Removed blog, other tools from nav bar

--- html/nav_search.php
+++ html/nav_search.php
@@ -3,9 +3,6 @@
 					<li><a href="<?php echo $url_html.'/index.php'; ?>">
 						<?php if ($page == 'search') {?><b><?php }?>Search<?php if ($page == 'search') {?></b><?php }?>
 					</a></li>
-					<li><a href="<?php echo $url_html.'/experimental.php'; ?>">
-						<?php if ($page == 'tools') {?><b><?php }?>Other Tools<?php if ($page == 'tools') {?></b><?php }?>
-					</a></li>
 					<li><a href="<?php echo $url_html.'/sources.php'; ?>">
 						<?php if ($page == 'sources') {?><b><?php }?>Sources<?php if ($page == 'sources') {?></b><?php }?>
 					</a></li>
@@ -15,6 +12,5 @@
 					<li><a href="<?php echo $url_html.'/help.php'; ?>">
 							<?php if ($page == 'help') {?><b><?php }?>Help<?php if ($page == 'help') {?></b><?php }?>
 					</a></li>					
-					<li><a href="http://tmv.westeurope.cloudapp.azure.com/blog" target="_blank">Blog</a></li>					
 				</ul>
 			</div>
