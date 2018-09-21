73a60093573e2ddba63f7dbfe1884d3b8d31c0cd
James Gawley <jamesgawley@Jamess-MacBook-Air.local>
2018-09-21 12:18:26 +0200

    Add fmsh logo

--- html/last.php
+++ html/last.php
@@ -4,6 +4,9 @@
                      <a href="http://buffalo.edu/"><img src="/images/ublogo.png" class="logo_wide" alt="University at Buffalo"></a>
                  </li>
                  <li>
+                     <a href="http://www.fmsh.fr/"><img src="/images/fmshlogo.png" class="logo_wide" alt="Fondation Maison des Siences de l'Homme"></a>
+                 </li>
+                 <li>
                      <a href="http://www.unive.it/pag/13526/"><img src="/images/cf-logo.png" class="logo_wide" alt="Ca' Foscari University of Venice"></a>
                  </li>
              </ul>
