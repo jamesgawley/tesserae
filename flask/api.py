d02e3708cec0dcd141c7e12214557bf8020d8b23
James Gawley <jamesgawley@Jamess-MacBook-Air.local>
2018-10-03 12:39:47 +0200

    Switch command strings to objects inserted into subroutine call

--- flask/api.py
+++ flask/api.py
@@ -21,15 +21,17 @@ def old_read_bin():
     result = subprocess.Popen(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", "tesresults", "--export", "html"], stdout=subprocess.PIPE)
     return result.stdout
 
-@app.route('/search/<target>/<source>/<format>')
+@app.route('/search/<target>/<source>/<unit>/<format>')
 def read_bin(target, source, format):
-    path = os.path.join('search', target, source)
+    path = os.path.join('tmp', target, source, unit)
     target_name = cts_list[target]
     source_name = cts_list[source]
     if not os.path.exists(path):
-        subprocess.Popen(" ".join(["perl", "/var/www/tesserae/cgi-bin/read_table.pl", "--target", target_name, "--source", source_name, "--binary", path]), shell=True)
-    result = subprocess.Popen(" ".join(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", path, "--export", format, "--window", "5"]), shell=True, stdout=subprocess.PIPE)
-    return result.stdout.read()
+        cmd = " ".join(["perl", "/var/www/tesserae/cgi-bin/read_table.pl", "--target", target_name, "--source", source_name, "--binary", path])
+        subprocess.Popen(cmd, shell=True)
+    cmd = " ".join(["perl", "/var/www/tesserae/cgi-bin/read_bin_tmv.pl", "--path", path, "--export", format, "--window", "5"])
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        .read()
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
